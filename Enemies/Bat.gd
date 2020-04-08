85c1d9748d692dc1919fe11f2493289aa3b1393b
Sean Hoekstra <sean.q.hoekstra@gmail.com>
2020-04-08 20:56:46 +0200

    added blue and green bat. they will be faster / poison you

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -3,6 +3,24 @@ extends KinematicBody2D
 var knockback = Vector2.ZERO
 var friction = 200
 
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	rng.randomize()
+	var my_random_number = rng.randi_range(0, 5)
+	if my_random_number == 0:
+		$AnimatedSprite.animation = 'FlyBlue'
+		print(my_random_number)
+	elif my_random_number in [1,2]:
+		$AnimatedSprite.animation = 'FlyGreen'
+		print(my_random_number)
+	else:
+		print(my_random_number)
+			
+	
+	
+		
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO,friction * delta)
 	knockback = move_and_slide(knockback)
@@ -11,3 +29,4 @@ func _on_Hurtbox_area_entered(area):
 	
 	knockback = area.knockback_vector * area.knockback_power
 	#queue_free()
+
