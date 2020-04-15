6a9514e24f61bcc9757769c1e9ac65245ab81d22
Sean Hoekstra <sean.q.hoekstra@gmail.com>
2020-04-15 22:36:46 +0200

    bats now chase and face the player

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,11 +2,25 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+#state machine
+enum{
+	IDLE,
+	WANDER,
+	CHASE	
+}
+
+export var acceleration = 300
+export var max_speed = 50
+export var friction = 200
+
 var knockback = Vector2.ZERO
-var friction = 200
 var rng = RandomNumberGenerator.new()
+var state = IDLE
+var velocity = Vector2.ZERO
 
 onready var stats = $Stats
+onready var player_detection_zone = $PlayerDetecctionZone
+onready var sprite = $AnimatedSprite
 
 func _ready():
 		
@@ -23,6 +37,30 @@ func _ready():
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO,friction * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+			seek_player()
+		
+		WANDER:
+			pass
+		
+		CHASE:
+			var player = player_detection_zone.player
+			if player != null:
+				#player position - bat position = the vector difference between the 2
+				#normalize the length so equalize the speed
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+				sprite.flip_h = velocity.x < 0	
+			else:
+				state = IDLE	
+	velocity = move_and_slide(velocity)
+		
+func seek_player():
+	if player_detection_zone.can_see_player():
+		state = CHASE
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
